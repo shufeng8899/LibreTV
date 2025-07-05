4e7eb2b7069fdc67b8eed2de27d3469ec188e69e
shufeng8899 <shujianfengvip@gmail.com>
2025-07-05 09:08:16 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -110,6 +110,16 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源'
     },
+    jkun: {
+         api: 'https://jkunzyapi.com',
+         name: 'jkun资源',
+         adult: true
+    },
+    haohua: {
+         api: 'https://hhzyapi.com/api.php/provide/vod',
+         name: '豪华资源',
+         adult: true
+    },
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
